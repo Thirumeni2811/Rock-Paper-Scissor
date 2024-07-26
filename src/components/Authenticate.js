cc1f69522ae4a00b82093577b56620c60c23b1c9
Thirumeni2811 <thirumeni948@gmail.com>
2024-07-26 17:25:33 +0530

    Twitter Authentication completed

--- src/components/Authenticate.js
+++ src/components/Authenticate.js
@@ -2,10 +2,11 @@ import React from 'react'
 import GoogleIcon from '@mui/icons-material/Google';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import { useNavigate } from 'react-router-dom';
-import { GoogleAuthProvider, signInWithPopup, FacebookAuthProvider } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithPopup, FacebookAuthProvider, TwitterAuthProvider } from 'firebase/auth';
 import { auth, db } from '../firebase';
 import { doc, setDoc } from 'firebase/firestore';
 import { toast } from 'react-toastify';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Authenticate = () => {
 
@@ -65,6 +66,35 @@ const Authenticate = () => {
             });
     }
 
+    //twitter
+    const Twitter = () => {
+        const provider = new TwitterAuthProvider
+
+        signInWithPopup(auth, provider)
+            .then(async(result) => {
+                console.log(result);
+                const user=result.user;
+                if (user) {
+                    await setDoc(doc(db, "User", user.uid), {
+                      Email: user.email,
+                      Name: user.displayName,
+                      Photo: user.photoURL
+                    });
+                    toast.success("Login Successfully !!", {
+                      position: "top-center",
+                    });
+                    navigate('/');
+                  }
+            })
+            .catch((error) => {
+                toast.error("Login Failed: " + error.message, {
+                  position: "bottom-center",
+                });
+                console.log("Error during Facebook Login: ", error);
+            });
+    }
+
+
   return (
     <>
         <section>
@@ -72,16 +102,24 @@ const Authenticate = () => {
                 <h1>-- Or continue with --</h1>
                 <div className='flex justify-around mt-4'>
                     <div 
+                        onClick={Facebook}
+                        className='border-4 border-solid lg:px-12 py-2 xs:px-8 sm:px-9 rounded-lg hover:border-luckyOrange hover:text-floralWhite'
+                    >
+                        <FacebookIcon />
+                    </div>
+
+                    <div 
                         onClick={Google}
-                        className='border-4 border-solid px-12 py-2 rounded-lg hover:border-luckyOrange hover:text-floralWhite'
+                        className='border-4 border-solid lg:px-12 py-2 xs:px-8 sm:px-9 rounded-lg hover:border-luckyOrange hover:text-floralWhite'
                     >
                         <GoogleIcon />
                     </div>
+                    
                     <div 
-                        onClick={Facebook}
-                        className='border-4 border-solid px-12 py-2 rounded-lg hover:border-luckyOrange hover:text-floralWhite'
+                        onClick={Twitter}
+                        className='border-4 border-solid lg:px-12 py-2 xs:px-8 sm:px-9 rounded-lg hover:border-luckyOrange hover:text-floralWhite'
                     >
-                        <FacebookIcon />
+                        <TwitterIcon />
                     </div>
                 </div>
             </div>
